300d0dbdda4f4f5bec29f11a47a9e02948833c5d
Clarissa Schwarz-Linnet <clarissa.schwarz@gmx.net>
2017-03-22 12:07:06 +0100

    First movebutton works and can be repeated

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 let alvarHero = document.querySelector("#hero");
-
-let jumpBtn = document.querySelector("#jumpbutton");
-
+let move_btn = document.querySelector("#move_btn");
 let btnOne = document.querySelector("#btn1");
 
-btnOne.addEventListener("click", moveAction);
+
+move_btn.addEventListener("click", moveAction);
 function moveAction(){
     alvarHero.classList.add("move");
+
 };
 
-/*jumpBtn.addEventListener("click", alvarmove);
-function alvarmove(){
-    console.log("im working");
-}*/
+
+
+alvarHero.addEventListener("animationend", removeClassJump);
+
+function removeClassJump(){
+    console.log ("I have to land");
+    alvarHero.classList.remove("move");
+}
+
+
