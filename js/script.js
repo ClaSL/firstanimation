776b10f7986d4486ea797e7c7c109aef39a367b4
Clarissa Schwarz-Linnet <clarissa.schwarz@gmx.net>
2017-03-23 21:48:36 +0100

    All buttons work now!

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 /*My hero*/
 let alvarHero = document.querySelector("#hero");
 
+/*SOUND*/
+
+let MovieSound = document.querySelector("#moviesound");
+let soundJumpBtn = document.querySelector("#playJump");
+
+soundJumpBtn.addEventListener("click", playAndJump);
+
+function playAndJump(){
+    console.log("Play and Jump")
+    MovieSound.addEventListener("ended", jumpHero);
+    MovieSound.play();
+}
+function jumpHero(){
+    alvarHero.classList.add("oneJump");
+}
+
 /*Move Button*/
 let move_btn = document.querySelector("#move_btn");
 
 move_btn.addEventListener("click", moveAction);
 
 function moveAction() {
-    alvarHero.classList.add("move");
+    alvarHero.classList.toggle("move");
 }
 
 /*Remove Move*/
@@ -82,7 +98,7 @@ function removeClassJump() {
 }
 
 /*Fade Button*/
-let fade = document.querySelector("#fade");
+let fade = document.querySelector("#fade_btn");
 
 fade.addEventListener("click", fadeHero);
 
@@ -101,8 +117,103 @@ function fadeRemove (){
     alvarHero.classList.remove("fadecss");
 }
 
-/*SOUND*/
 
-let MovieSound = document.querySelector("#moviesound");
-MovieSound.addEventListener("ended", fadeHero);
+/*SCALE BUTTON*/
 
+let scaleing = document.querySelector("#scale_btn");
+
+scaleing.addEventListener("click", startScale);
+
+function startScale (){
+    console.log("I scale")
+    alvarHero.classList.add("scaleanimation");
+}
+
+/*Remove Scale Button*/
+
+alvarHero.addEventListener("animationend", scaleRemove);
+
+function scaleRemove () {
+    console.log ("scale stop");
+    alvarHero.classList.remove("scaleanimation");
+}
+
+/*Falldown*/
+
+let fallingDown = document.querySelector("#falldown_btn");
+
+fallingDown.addEventListener("click", startFall);
+
+function startFall () {
+    console.log ("I fall!");
+    alvarHero.classList.add("fall");
+
+}
+
+/*remove Falldown*/
+
+alvarHero.addEventListener("animationend", removeFall);
+
+function removeFall (){
+    console.log("I am back");
+    alvarHero.classList.remove("fall");
+}
+
+/*SHAKE*/
+
+let heroShake = document.querySelector("#shake_btn");
+
+heroShake.addEventListener("click", startShake);
+
+function startShake (){
+    console.log("I am shakeing");
+    alvarHero.classList.add("shake");
+}
+
+/*remove shake*/
+
+alvarHero.addEventListener("animationend", removeShake);
+
+function removeShake (){
+    console.log("I stop to shake");
+    alvarHero.classList.remove("shake");
+}
+
+/*GLOW*/
+let heroGlow = document.querySelector("#glow_btn")
+
+heroGlow.addEventListener("click", startGlow);
+
+function startGlow (){
+    console.log("I glow");
+    alvarHero.classList.add("heroglowing");
+}
+
+/*remove GLOW*/
+
+alvarHero.addEventListener("animationend", removeGlow);
+
+function removeGlow (){
+    console.log("I stop glowing");
+    alvarHero.classList.remove("heroglowing");
+}
+
+/*MIRROR*/
+
+let heroMirror = document.querySelector("#mirror_btn")
+
+heroMirror.addEventListener("click", startMirror);
+
+function  startMirror (){
+    console.log("Mirror me");
+    alvarHero.classList.add("mirrorhero");
+}
+
+/*remove MIRROR*/
+
+alvarHero.addEventListener("animationend", removeMirror);
+
+function removeMirror (){
+    console.log("I stop mirroring");
+    alvarHero.classList.remove("mirrorhero");
+}
